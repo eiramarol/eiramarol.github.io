a1ac29deb9944ad194bf1c4b394fa4180b608983
halaliel <0halaliel0@gmail.com>
2016-07-11 06:45:30 +0800

    Added some js

--- js/cats.js
+++ js/cats.js
@@ -1,8 +1,29 @@
-$(document).ready(function(e) {
+$(document).ready(function() {
+
+        $(".h1Style,#ffc,#ghub,#linkedin,h4").hide();
+
+        $(".h1Style").fadeIn(5000);
+        $("#ffc").fadeIn(6000);
+        $("#ghub").fadeIn(7000);
+        $("#linkedin").fadeIn(8000);
+        $("h4").fadeIn(8500);
+
+
+// button hover
     $("button").hover(function() {
         $("#"+$(this).attr("id")).css('background','rgba(255,255,255,1)');
     }, function() {
         $("#"+$(this).attr("id")).css('background','rgba(255,255,255,.7)');
     });
 
+// collapse BG
+        // $(".collapseBtn").click( function() {
+        //     if ($(window)) {
+        //         // statement
+        //     }
+        //     $(".navbar").toggleClass('collapseColor');
+        // });
+
 });
+
+
