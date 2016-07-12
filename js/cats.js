bcf0f459cfa1352a913c99ae8851d58eef9d3f76
halaliel <0halaliel0@gmail.com>
2016-07-12 08:34:37 +0800

    updated work tab
    -added thumbnails
    -updated stylesheet
    -comment out js f

--- js/cats.js
+++ js/cats.js
@@ -1,23 +1,32 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-        $(".h1Style,#ffc,#ghub,#linkedin,h4").hide();
+//         $(".h1Style,h4").hide();
 
-        $(".h1Style").fadeIn(5000);
-        $("#ffc").fadeIn(6000);
-        $("#ghub").fadeIn(7000);
-        $("#linkedin").fadeIn(8000);
-        $("h4").fadeIn(8500);
-
-
-// button hover
-    $("button").hover(function() {
-        $("#"+$(this).attr("id")).css('background','rgba(255,255,255,1)');
-    }, function() {
-        $("#"+$(this).attr("id")).css('background','rgba(255,255,255,.7)');
-    });
+//         $(".h1Style").fadeIn(5000);
+//         $("#ffc").fadeIn(6000);
+//         $("#ghub").fadeIn(7000);
+//         $("#linkedin").fadeIn(8000);
+//         $("#behance").fadeIn(9000);
+//         $("#behance").animate({left: "552"}, 2000);
+//         $("h4").fadeIn(5500);
 
 
+// // button hover
+//     $("button").hover(function() {
+//         $("#"+$(this).attr("id")).css('background','rgba(255,255,255,1)');
+//     }, function() {
+//         $("#"+$(this).attr("id")).css('background','rgba(255,255,255,.7)');
+//     });
 
-});
 
+
+// });
+// $(document).ready(function(){
+//     if (true) {};
+//     $(".navbar").hover(function(){
+//         $("#collapseNavbarNav").collapse('show');
+//     },function(){
+//         $("#collapseNavbarNav").collapse('hide');
+//     });
+// });
 
